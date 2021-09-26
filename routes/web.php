8c3c0483eff73fd5f72cd6a93e0f27e0dc5466be
AkbarMungkas <3103119010@student.smktelkom-pwt.sch.id>
2021-09-26 19:24:05 +0700

    menambah view dan controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeUnit\FunctionUnit;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,43 @@ use SebastianBergmann\CodeUnit\FunctionUnit;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/me', function(){
+//     return "Pamungkas";
+// });
+
+// Route::get('/me', function(){
+//     return ["Pamungkas", "kk4", "12rpl1"];
+// });
+
+// Route::get('/me', function(){
+//     return[
+//         'nama' => 'Pamungkas',
+//         'nis' => 3103119010,
+//         'kelas' => 'XII RPL 1',
+//         'Nomor HP' => 1112222333
+//     ];
+// });
+
+
+
+// Route::get('/me', function(){
+//     return response()->json(
+//         [
+//         'nama' => 'Pamungkas',
+//         'nis' => 3103119010,
+//         'kelas' => 'XII RPL 1',
+//         'Nomor HP' => 1112222333
+//         ],
+//     );
+// });
+
+Route::get('/back', function(){
+    return redirect('/');
 });
+
+// Route::get('/me', [AuthController::class, 'me']);
+Route::get('/me', 'App\Http\Controllers\AuthController@me');
